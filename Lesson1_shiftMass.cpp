6404dc79d37bb212326653bc54c1c4f3654835cb
Dmitry Terentev <zabey52@gmail.com>
2022-11-09 23:03:17 +0300

    2

--- Lesson1_shiftMass.cpp
+++ Lesson1_shiftMass.cpp
@@ -13,16 +13,52 @@ int* create_arr(const int size_row)
 }
 
 // Функция печати массива
-void print_arr(int* arr, const int size_row)
+void print_arr(int* arr, const int arr_size_row)
 {
     std::cout << "[";
-    for (int i = 0; i < size_row - 1; i++)
+    for (int i = 0; i < arr_size_row - 1; i++)
     {
         std::cout << arr[i] << ", ";
     }
-    std::cout << arr[size_row - 1];
+    std::cout << arr[arr_size_row - 1];
     std::cout << "]" << std::endl;
 }
+
+// Заполняем массив из файла
+void fill_arr(int* arr, const int arr_size_row, int cursor_position)
+{
+    std::ifstream fileIn("in.txt");
+    fileIn.seekg(cursor_position);
+
+    for (int i = 0; i < arr_size_row; i++)
+    {
+        fileIn >> arr[i];
+    }
+    fileIn.close();   
+}
+
+// Функция сдвига массива влево
+void shiftLeft_arr(int* arr, const int arr_size_row)
+{
+    int tmp = arr[0];
+    for (int i = 0; i < arr_size_row - 1; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
+    arr[arr_size_row - 1] = tmp;
+}
+
+// Функция сдвига массива вправо
+void shiftRight_arr(int* arr, const int arr_size_row)
+{
+    int tmp = arr[arr_size_row - 1];
+    for (int i = arr_size_row - 1; i >= 1; i--)
+    {
+        arr[i] = arr[i - 1];
+    }
+    arr[0] = tmp;
+}
+
 
 
 
@@ -33,7 +69,7 @@ int main()
     std::ifstream fileIn("in.txt"); // Открываем файл и проверяем удалось ли открыть
     if (!fileIn)
     {
-        std::cout << "Не удалось открыть файл in.txt" << std::endl;
+        std::cout << "Ошибка открытия файла in.txt" << std::endl;
         return 1;
     }
     else
@@ -45,19 +81,48 @@ int main()
 
     int arr1_size_row = 0;
 
-    fileIn >> arr1_size_row; // Считываем размер массива
-
-    int* arr1 = create_arr(arr1_size_row);
-
-    std::cout << "Размер массива: " << arr1_size_row << ". " << std::endl << "Массив из файла:\t";
-    
-    print_arr(arr1, arr1_size_row);
+    fileIn >> arr1_size_row; // Считываем из файла размер массива 1.
+    int cursor_position = fileIn.tellg();
+    std::cout << "cursor_position1: " << fileIn.tellg() << ". " << std::endl;
     
 
-    std::cout << "Массив со сдвигом:\t";
+    int* arr1 = create_arr(arr1_size_row); //создаем динамический массив 1.
 
+    std::cout << "Размер массива 1: " << arr1_size_row << ". " << std::endl << "Массив 1 из файла:\t\t";
+    
+    fill_arr(arr1, arr1_size_row, cursor_position);
+    print_arr(arr1, arr1_size_row); //печатаем исходный массив 1.
+    
+    std::cout << "Массив 1 со сдвигом влево:\t";
+    shiftLeft_arr(arr1, arr1_size_row);
     print_arr(arr1, arr1_size_row);
 
+    int arr2_size_row = 0;
+    
+    fileIn.seekg(arr1_size_row);
+    int cursor_position2 = fileIn.tellg();
+    
+    std::cout << "cursor_position2: " << fileIn.tellg() << ". " << cursor_position2 << std::endl;
+    fileIn >> arr2_size_row; // Считываем из файла размер массива 2.
+    
+
+    std::cout << "arr2_size_row: " << arr2_size_row << ". " << std::endl;
+  
+
+
+
+
+
+
+    /*std::cout << "Массив 2 со сдвигом вправо:\t";
+    shiftRight_arr(arr1, arr1_size_row);
+    print_arr(arr1, arr1_size_row);*/
+
+
+
+    fileIn.close();
+    delete[] arr1; // Удаление динамического массива
+    //delete[] arr2; // Удаление динамического массива
 
 
 }
